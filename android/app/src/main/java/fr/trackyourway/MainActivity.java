9d5dba69f257d75544823689db6e6bba9354ee37
Bab <bastienchapellier@gmail.com>
2016-10-19 11:55:01 +0100

    Add activities

--- android/app/src/main/java/fr/trackyourway/MainActivity.java
+++ android/app/src/main/java/fr/trackyourway/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.trackyourway;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * Runner
+         */
+        Button buttonRunner = (Button) findViewById(R.id.runner);
+        buttonRunner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RunnerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /**
+         * Viewer
+         */
+        Button buttonViewer = (Button) findViewById(R.id.viewer);
+        buttonViewer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewerActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
