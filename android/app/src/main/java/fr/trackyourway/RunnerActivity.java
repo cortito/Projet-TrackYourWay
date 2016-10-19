f881abe38ec6a804aab249a4648ff73743121666
Unknown <corto.adiba@gmail.com>
2016-10-19 12:12:18 +0200

    ajout permissions dans manifest et api google

--- android/app/src/main/java/fr/trackyourway/RunnerActivity.java
+++ android/app/src/main/java/fr/trackyourway/RunnerActivity.java
@@ -1,13 +1,58 @@
 package fr.trackyourway;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-public class RunnerActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class RunnerActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient = null;
+    Location mLastLocation = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_runner);
+
+        try {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            Log.d("TAG","Connected");
+
+        }catch (SecurityException e){
+            Log.e("PERMISSION EXCEPTION","PERMISSON_NOT_GRANTED");
+        }
+
+        if (mLastLocation != null) {
+            Log.d("GET LOCATION","ça marche");
+            TextView mLatitudeText  = (TextView) findViewById(R.id.latitude);
+            TextView mLongitudeText = (TextView) findViewById(R.id.longitude);
+
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
