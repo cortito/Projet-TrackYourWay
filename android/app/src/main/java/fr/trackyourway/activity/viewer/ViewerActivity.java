32b6dadb62ebea545d121a84f695c3ae5f806b1b
Bab <bastienchapellier@gmail.com>
2016-10-25 10:54:08 +0100

    Adding Filter

--- android/app/src/main/java/fr/trackyourway/activity/viewer/ViewerActivity.java
+++ android/app/src/main/java/fr/trackyourway/activity/viewer/ViewerActivity.java
@@ -2,6 +2,10 @@ package fr.trackyourway.activity.viewer;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +14,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 import fr.trackyourway.R;
+import fr.trackyourway.business.alertdialog.FilterBibDialog;
 import fr.trackyourway.business.services.RetrieveRunnerTimerTask;
 
 import static fr.trackyourway.R.id.map;
@@ -20,6 +25,7 @@ public class ViewerActivity extends FragmentActivity implements OnMapReadyCallba
 
     private GoogleMap mMap;
     private RetrieveRunnerTimerTask retrieveRunnerTimerTask;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,37 @@ public class ViewerActivity extends FragmentActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(map);
         mapFragment.getMapAsync(this);
+
+        /**
+         * Fill up the spinner
+         */
+        spinner = (Spinner) findViewById(R.id.spinnerFilter);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.filterStringArray, R.layout.spinner_style);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 1:
+                        FilterBibDialog alertDialog = new FilterBibDialog();
+                        alertDialog.show(getFragmentManager(), TAG);
+                        break;
+                    case 2:
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
---
+++ android/app/src/main/java/fr/trackyourway/business/alertdialog/FilterBibDialog.java
@@ -0,0 +1,41 @@
+package fr.trackyourway.business.alertdialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import fr.trackyourway.R;
+
+/**
+ * Created by bab on 25/10/16.
+ */
+
+public class FilterBibDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_bib_filter, null))
+                .setMessage("Filtering by bib")
+                .setPositiveButton("Filter", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
