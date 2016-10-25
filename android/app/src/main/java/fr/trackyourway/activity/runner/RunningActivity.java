4f9fd58054b76a1f70cd81f09584faaeceb1f711
Unknown <corto.adiba@gmail.com>
2016-10-25 14:43:51 +0200

    Upatdates for LocationRequest

--- android/app/src/main/java/fr/trackyourway/activity/runner/RunningActivity.java
+++ android/app/src/main/java/fr/trackyourway/activity/runner/RunningActivity.java
@@ -1,19 +1,24 @@
 package fr.trackyourway.activity.runner;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import fr.trackyourway.R;
@@ -24,12 +29,14 @@ import okhttp3.OkHttpClient;
 public class RunningActivity extends AppCompatActivity implements ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, AsyncSendingRunnerTask.RunnerSendListener {
 
 
+    public final static int UPDATE_RATE = 5000;
     GoogleApiClient mGoogleApiClient = null;
 
-    private Location mLastLocation = null;
+    private Location mCurrentLocation = null;
 
     //private AsyncSendingRunnerTask asyncSendingRunnerTask;
     private SendingRunnerTimerTask sendingRunnerTimerTask;
+    LocationRequest locationRequest;
 
     //String mLastUpdateTime = null;
 
@@ -47,7 +54,7 @@ public class RunningActivity extends AppCompatActivity implements ConnectionCall
             @Override
             public void onClick(View v) {
                 mGoogleApiClient.disconnect();
-                Log.d("tag","going to alert");
+                Log.d("tag", "going to alert");
                 if (!mGoogleApiClient.isConnected()) {
                     Intent intent = new Intent(RunningActivity.this, RunnerActivity.class);
                     startActivity(intent);
@@ -62,17 +69,18 @@ public class RunningActivity extends AppCompatActivity implements ConnectionCall
                     .addApi(LocationServices.API)
                     .build();
         }
+        locationRequest = new LocationRequest();
+        createLocationRequest(locationRequest);
 
 
     }
 
-    /*protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
+    protected void createLocationRequest(LocationRequest mLocationRequest) {
+        mLocationRequest.setInterval(UPDATE_RATE);
+        mLocationRequest.setFastestInterval(UPDATE_RATE);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-    }*/
+    }
 
     protected void onStart() {
         mGoogleApiClient.connect();
@@ -88,69 +96,50 @@ public class RunningActivity extends AppCompatActivity implements ConnectionCall
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
-        try {
+        /*try {
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                     mGoogleApiClient);
             Log.d("TAG", "Connected");
 
         } catch (SecurityException e) {
             Log.e("PERMISSION EXCEPTION", "PERMISSON_NOT_GRANTED");
-        }
-
-        sendingRunnerTimerTask = new SendingRunnerTimerTask(mLastLocation);
-        sendingRunnerTimerTask.start();
-
-      // TODO HERE
-
-        //asyncSendingRunnerTask = new AsyncSendingRunnerTask(this);
-        /*try {
-            sendRunner();
-        } catch (IOException e) {
-            e.printStackTrace();
         }*/
+        requestLocationUpdates();
     }
 
-    /*void sendRunner() throws IOException {
-        if (asyncSendingRunnerTask != null) {
-            asyncSendingRunnerTask.execute(mLastLocation);
-
+    private void requestLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
         }
-    }*/
+
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
         sendingRunnerTimerTask.onPause();
-        //asyncSendingRunnerTask.cancel(true);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Toast.makeText(this, "Connection suspended", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Toast.makeText(this, "Connection Failed ...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onLocationChanged(Location location) {
 
-       /* mCurrentLocation = location;
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        updateUI();*/
+        mCurrentLocation = location;
+        sendingRunnerTimerTask = new SendingRunnerTimerTask(mCurrentLocation);
+        sendingRunnerTimerTask.start();
+
+        Toast.makeText(this, "Location sent...", Toast.LENGTH_SHORT).show();
 
     }
-
-    private void updateUI() {
-       /* mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
-        mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
-        //mLastUpdateTimeTextView.setText(mLastUpdateTime);*/
-    }
-
-
-
     @Override
     public void onRunnerRunning(String o) {
 
